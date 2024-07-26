be39b37323b764f8a84d83b5242c95d1db89b742
SANJIT-techie <86309437+SANJIT-techie@users.noreply.github.com>
2024-07-26 09:37:20 +0000

    added the variables file

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,23 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2 // not allowed
+
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+
+console.log(accountEmail);
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+so their was no concept of the global or functional variable, the variable got modified globally.
+*/
+
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
