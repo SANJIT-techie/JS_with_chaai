3bb77bb09b700136b6e791c9ae66f436af969531
SANJIT-techie <86309437+SANJIT-techie@users.noreply.github.com>
2024-08-06 06:07:30 +0000

    updated the changes as per learning

---
+++ test.js
@@ -0,0 +1,32 @@
+// const name = "sanjit";
+// name = "bisesh"
+
+// console.log(typeof name);
+
+// const newName = new String ("Sanjib")
+// console.log(newName);
+// console.log(newName.toUpperCase());
+// console.log(newName);
+
+
+// let myYoutubeName = "helloHItesh"
+
+// let anotherName = myYoutubeName
+// anotherName = "newName"
+
+// console.log(myYoutubeName);
+// console.log(anotherName);
+
+
+let user = {
+    email: "sanjit.s@bourn.com",
+    upi: "user@ybl"
+}
+
+let userTwo = user
+userTwo.email = "pappu@bourn.com"
+
+console.log(user);
+console.log(userTwo.email);
+
+
